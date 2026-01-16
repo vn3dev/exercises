529c8feda068e370828ae964f97c17d528cf797a
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-01-16 18:19:44 -0300

    add grep search term variables using an array

--- bash/logs-exercise-1/analyse-logs.sh
+++ bash/logs-exercise-1/analyse-logs.sh
@@ -4,23 +4,29 @@ LOG_DIR="./Logs"
 APP_LOG_FILE="application.log"
 SYS_LOG_FILE="system.log"
 
+ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+
 echo -e "\nAnalysing log files"
 echo "===================" 
 
-echo -e "\nFind all log files from yesterday"
-find $LOG_DIR -name "*.log" -mtime -1
+echo -e "\nList of all log files updated yesterday"
+LOG_FILES=$(find $LOG_DIR -name "*.log" -mtime -1)
+echo $LOG_FILES
 
 echo -e "\nSearch for ERROR in application.log file"
-grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
-echo -e "\nCount how many errors are in the application.log file"
-grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE"
-echo -e "\nCount how many fatals are in the application.log file"
-grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE"
+grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
+echo -e "\nCount how many errors are in the application.log file"
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+
+echo -e "\nCount how many fatals are in the application.log file"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nCount how many fatals are in the system.log file"
-grep -c "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
+
 echo -e "\nCount how many criticals are in the system.log file"
-grep -c "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+
 echo -e "\nSearch for CRITICAL in system.log file"
-grep "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
+grep "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
