4643fb6fb20495d3596526c5976b94e13ab32e29
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-01-16 18:47:01 -0300

    create reports directory and send analysis to ./reports/log_analysis_report.txt

--- bash/logs-exercise-1/analyse-logs.sh
+++ bash/logs-exercise-1/analyse-logs.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
 LOG_DIR="./Logs"
-
 ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+REPORT_FILE="./Logs/reports/log_analysis_report.txt"
 
-echo -e "\nAnalysing log files"
-echo "===================" 
+echo -e "\nAnalysing log files" > "$REPORT_FILE"
+echo "===================" >> "$REPORT_FILE"
 
-echo -e "\nList of all log files updated yesterday"
+echo -e "\nList of all log files updated yesterday" >> "$REPORT_FILE"
 LOG_FILE=$(find $LOG_DIR -name "*.log" -mtime -1)
-echo $LOG_FILE
+echo "$LOG_FILE" >> "$REPORT_FILE"
 
 for LOG_FILE in $LOG_FILE; do
 
-    echo -e "\n"
-    echo "====================================="
-    echo "==========$LOG_FILE=========="
-    echo "====================================="
+    echo -e "\n" >> "$REPORT_FILE"
+    echo "=====================================" >> "$REPORT_FILE"
+    echo "==========$LOG_FILE==========" >> "$REPORT_FILE"
+    echo "=====================================" >> "$REPORT_FILE"
 
     for PATTERN in ${ERROR_PATTERNS[@]}; do
 
-        echo -e "\nCount how many $PATTERN are in the $LOG_FILE file"
-        grep -c "$PATTERN" "$LOG_FILE"
+        echo -e "\nCount how many $PATTERN are in the $LOG_FILE file" >> "$REPORT_FILE"
+        grep -c "$PATTERN" "$LOG_FILE" >> "$REPORT_FILE"
 
-        echo -e "\nSearch for $PATTERN in $LOG_FILE file"
-        grep "$PATTERN" "$LOG_FILE"
+        echo -e "\nSearch for $PATTERN in $LOG_FILE file" >> "$REPORT_FILE"
+        grep "$PATTERN" "$LOG_FILE" >> "$REPORT_FILE"
 
     done
 done
+
+echo -e "\nLog analysis completed and report saved in: $REPORT_FILE"
