332a003803034bcb8d60c24f8cf314512ede9c22
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-01-16 18:02:58 -0300

    add shebang and echoes

--- bash/logs-exercise-1/analyse-logs.sh
+++ bash/logs-exercise-1/analyse-logs.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+
+echo "Analysing log files"
+echo "===================" 
+
+echo "Find all log files from yesterday"
 find ./Logs -name "*.log" -mtime -1
 
+echo "Search for ERROR in application.log file"
 grep "ERROR" Logs/application.log
+echo "Count how many errors are in the application.log file"
 grep -c "ERROR" Logs/application.log
+echo "Count how many fatals are in the application.log file"
 grep -c "FATAL" Logs/application.log
 
+
+echo "Count how many fatals are in the system.log file"
 grep -c "FATAL" Logs/system.log
+echo "Count how many criticals are in the system.log file"
 grep -c "CRITICAL" Logs/system.log
+echo "Search for CRITICAL in system.log file"
 grep "CRITICAL" Logs/system.log
