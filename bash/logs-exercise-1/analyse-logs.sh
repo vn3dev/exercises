0f9f34a2ab750f3571d6f5536427ef138a3937fa
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-01-16 18:32:17 -0300

    add dynamic logic using a loop to search for error patterns in each log file

--- bash/logs-exercise-1/analyse-logs.sh
+++ bash/logs-exercise-1/analyse-logs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 LOG_DIR="./Logs"
-APP_LOG_FILE="application.log"
-SYS_LOG_FILE="system.log"
 
 ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
 
@@ -10,23 +8,25 @@ echo -e "\nAnalysing log files"
 echo "===================" 
 
 echo -e "\nList of all log files updated yesterday"
-LOG_FILES=$(find $LOG_DIR -name "*.log" -mtime -1)
-echo $LOG_FILES
+LOG_FILE=$(find $LOG_DIR -name "*.log" -mtime -1)
+echo $LOG_FILE
 
-echo -e "\nSearch for ERROR in application.log file"
-grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+for LOG_FILE in $LOG_FILE; do
+    echo -e "\nCount how many ${ERROR_PATTERNS[0]} are in the $LOG_FILE file"
+    grep -c "${ERROR_PATTERNS[0]}" "$LOG_FILE"
 
-echo -e "\nCount how many errors are in the application.log file"
-grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+    echo -e "\nSearch for ${ERROR_PATTERNS[0]} in $LOG_FILE file"
+    grep "${ERROR_PATTERNS[0]}" "$LOG_FILE"
 
-echo -e "\nCount how many fatals are in the application.log file"
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
+    echo -e "\nCount how many ${ERROR_PATTERNS[1]} are in the $LOG_FILE file"
+    grep -c "${ERROR_PATTERNS[1]}" "$LOG_FILE"
 
-echo -e "\nCount how many fatals are in the system.log file"
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$SYS_LOG_FILE"
+    echo -e "\nSearch for ${ERROR_PATTERNS[1]} in $LOG_FILE file"
+    grep "${ERROR_PATTERNS[1]}" "$LOG_FILE"
 
-echo -e "\nCount how many criticals are in the system.log file"
-grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+    echo -e "\nCount how many ${ERROR_PATTERNS[2]} are in the $LOG_FILE file"
+    grep -c "${ERROR_PATTERNS[2]}" "$LOG_FILE"
 
-echo -e "\nSearch for CRITICAL in system.log file"
-grep "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+    echo -e "\nSearch for ${ERROR_PATTERNS[2]} in $LOG_FILE file"
+    grep "${ERROR_PATTERNS[2]}" "$LOG_FILE"
+done
