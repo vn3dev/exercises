187a2876e6e91b9b5bead701f567811e053e3f1e
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-01-16 18:13:16 -0300

    add variables to directories and log files

--- bash/logs-exercise-1/analyse-logs.sh
+++ bash/logs-exercise-1/analyse-logs.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+LOG_DIR="./Logs"
+APP_LOG_FILE="application.log"
+SYS_LOG_FILE="system.log"
+
 echo -e "\nAnalysing log files"
 echo "===================" 
 
 echo -e "\nFind all log files from yesterday"
-find ./Logs -name "*.log" -mtime -1
+find $LOG_DIR -name "*.log" -mtime -1
 
 echo -e "\nSearch for ERROR in application.log file"
-grep "ERROR" Logs/application.log
+grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
 echo -e "\nCount how many errors are in the application.log file"
-grep -c "ERROR" Logs/application.log
+grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE"
 echo -e "\nCount how many fatals are in the application.log file"
-grep -c "FATAL" Logs/application.log
+grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE"
 
 
 echo -e "\nCount how many fatals are in the system.log file"
-grep -c "FATAL" Logs/system.log
+grep -c "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
 echo -e "\nCount how many criticals are in the system.log file"
-grep -c "CRITICAL" Logs/system.log
+grep -c "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
 echo -e "\nSearch for CRITICAL in system.log file"
-grep "CRITICAL" Logs/system.log
+grep "CRITICAL" "$LOG_DIR/$SYS_LOG_FILE"
