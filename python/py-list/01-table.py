9734ea4a7dd5deedf256b9195b3dd18bd38f1352
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-01-23 04:09:01 -0300

    add comments to every line

--- python/py-list/01-table.py
+++ python/py-list/01-table.py
@@ -3,17 +3,17 @@
 # criar function que multiplica o int por i 10x
 # executar chain de validação com input
 
-def multiplicar(inteiro):
-    for i in range (1, 11):
-        print(f"{inteiro} vezes {i}: {inteiro * i}")
+def multiplicar(inteiro): # function que vai calcular
+    for i in range (1, 11): # loop for em py para i entre 1 e 10
+        print(f"{inteiro} vezes {i}: {inteiro * i}") # imprime resultado
 
-def validar():
-    try:
-        user_input_number = int(user_input)
-        multiplicar(user_input_number)
-    except ValueError:
-        print("valor invalido, deve ser um inteiro\n")
+def validar(): # function para validar se input é um inteiro
+    try: # se retornar erro, ele n executa e vai para except
+        user_input_number = int(user_input) # define uma variavel para receber o valor do input
+        multiplicar(user_input_number) # chama function usando parametro do input
+    except ValueError: # executa se acima falhar
+        print("valor invalido, deve ser um inteiro\n") # imprime erro
 
 
-user_input = input("insira um numero inteiro\n")
-validar()
+user_input = input("insira um numero inteiro\n") # input pro usuario
+validar() # chama function validar
