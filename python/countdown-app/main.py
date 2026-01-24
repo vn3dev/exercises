a290052d9f40684c5038dc36b0c09a6305b8f680
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-01-24 01:58:34 -0300

    create .py files and add program expected logic

---
+++ python/countdown-app/main.py
@@ -0,0 +1,7 @@
+# fazer um programa que receba um input "objetivo:data" - "aprender python:02/03/2026" e transformar isso em um countdown de dias
+# ate esse objetivo. (Time remaining for your goal: learn python is X days)
+
+# modularizar e criar logica input base
+# multiplicar meses/anos para dias
+# somar tudo e exibir para o usuario
+
