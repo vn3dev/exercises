59cfed19ad675f7d389ede9b248c1337db7e2fd4
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-02-05 12:32:19 -0300

    add comments to socket script

--- python/socket/socket.py
+++ python/socket/socket.py
@@ -2,17 +2,22 @@
 
 import socket
 
+# cria um socket TCP, AF_INET usa ipv4 e SOCK_STREAM para TCP
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# binda o socket no endereço
 socket.bind(("localhost", 9473))
+# coloca o socket em listening
 socket.listen()
 
+# espera um cliente conectar
 conn, addr = socket.accept()
 
 while True:
-    data = conn.recv(1024)
+    data = conn.recv(1024) # recebe dados do cliente, até 1024kbs
     if not data:
         break
     print("Nova mensagem do host %s: %s" % (addr, data.decode()))
 
+# fecha conexão e libera recursos
 conn.close()
