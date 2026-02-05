b2a8f8474eb3daf08a00543e23edb8a41c148641
Vinicius Costa <vinicosta.vn3dev@gmail.com>
2026-02-05 12:44:14 -0300

    add comments to client socket script

--- python/socket/client.py
+++ python/socket/client.py
@@ -2,9 +2,12 @@
 
 import socket
 
+# cria o socket com ipv4 e TCP
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tenta conectar, só funciona se o outro socket estiver escutando
 socket.connect(("localhost", 9473))
 
 while True:
     msg = input("Digite aqui a sua mensagem: ")
+    # transforma a string em bytes e envia
     socket.send(msg.encode())
